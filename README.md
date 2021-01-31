431934a85e88da1aad350bd55c5f8f6ab0f9674c
wwwshwww <www.shinderu.www@gmail.com>
2021-01-31 08:13:06 +0000

    0.1.1

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # roomor: A framework for autogenerating and deploying random room simulation for Gazebo
 
-[![MIT License](https://img.shields.io/badge/license-MIT-green)](https://github.com/wwwshwww/roomor/LICENSE)
+[![MIT License](https://img.shields.io/badge/license-MIT-green)](https://github.com/wwwshwww/roomor/blob/master/LICENSE)
 [![PyPI](https://img.shields.io/pypi/v/roomor)](https://pypi.org/project/roomor/)
 ![PyPI - Python Version](https://shields.io/pypi/pyversions/roomor)
+
+## Installation
+
+Install using pip:
+
+```
+$ pip install roomor
+```
--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ with open(version_file, 'r') as f:
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
     
-requirements_required = set([
+requirements = [
     'numpy-quaternion',
     'trimesh[easy]',
     'shapely',
     'scikit-learn',
     'opencv-python',
     'pcg_gazebo'
-])
+]
 
 setuptools.setup(
     name='roomor',
@@ -25,15 +25,15 @@ setuptools.setup(
     description='A Python framework for generating and deploying random room for Gazebo',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='wwwshwww',
+    author='Reona Sato',
     author_email='www.shinderu.www@gmail.com',
     url='https://github.com/wwwshwww/roomor',
     packages=setuptools.find_packages(),
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='gazebo ros simulation robotics autogeneration environment reinforcement-learning pcg_gazebo',
-    install_requires=list(requirements_required)
+    install_requires=requirements
 )
